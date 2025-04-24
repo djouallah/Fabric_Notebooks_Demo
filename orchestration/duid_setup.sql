160acfe8c57b13ad0f34eba3fa310cb36aa05b0f
Mimoune <djouallah@users.noreply.github.com>
2025-04-24 10:51:17 +1000

    Delete orchestration/duid_setup.sql

--- orchestration/duid_setup.sql
+++
@@ -1,9 +0,0 @@
--- materialized: (aemo,duid,ignore)
-CREATE TEMP TABLE IF NOT EXISTS duid_temp(
-  DUID VARCHAR,
-  Region VARCHAR,
-  FuelSourceDescriptor VARCHAR,
-  Participant VARCHAR,
-  State VARCHAR
-);
-select * from duid_temp
