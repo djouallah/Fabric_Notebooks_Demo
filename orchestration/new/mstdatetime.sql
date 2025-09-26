fe238f85132f9908fd536a7a90c1e73e1a985a39
Mimoune <djouallah@users.noreply.github.com>
2025-09-26 23:09:17 +1000

    Create mstdatetime.sql

---
+++ orchestration/new/mstdatetime.sql
@@ -0,0 +1,8 @@
+WITH data AS (
+    SELECT
+        UNNEST(GENERATE_SERIES('2023-01-01 00:00:00'::TIMESTAMPTZ, '2023-01-01 23:55:00'::TIMESTAMPTZ, INTERVAL 5 MINUTE)) AS interval_time
+)
+SELECT
+    CAST(STRFTIME(interval_time, '%H%M') AS INT16) AS time,
+    CAST(STRFTIME(interval_time, '%H') AS INT16) AS hour
+FROM data;
