2775249880650e06be07adf8b0014c27cba48681
Mimoune <djouallah@users.noreply.github.com>
2025-09-27 17:07:56 +1000

    Update summary__backfill.sql

--- orchestration/new/summary__backfill.sql
+++ orchestration/new/summary__backfill.sql
@@ -3,8 +3,8 @@
         cast(strftime(s.SETTLEMENTDATE, '%H%M') AS INT16)                       as time ,
         (select max(cast(settlementdate as TIMESTAMPTZ) ) from scada)           as cutoff ,
         s.DUID,
-        max(s.INITIALMW)                                                        as mw,
-        max(p.RRP)                                                              as price
+        CAST(max(s.INITIALMW)    AS DECIMAL(18, 4)))                            as mw,
+        CAST(max(p.RRP)          AS DECIMAL(18, 4))                             as price
       from  scada   s
             LEFT JOIN duid d    ON s.DUID = d.DUID
             LEFT JOIN price   p ON s.SETTLEMENTDATE = p.SETTLEMENTDATE AND d.Region = p.REGIONID
